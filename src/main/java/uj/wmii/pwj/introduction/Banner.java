a1536429895d742c7206e4fc922c76f44f7f42c0
emiliabiros <birosemilia@gmail.com>
2025-10-07 18:12:39 +0200

    all tests passed

--- src/main/java/uj/wmii/pwj/introduction/Banner.java
+++ src/main/java/uj/wmii/pwj/introduction/Banner.java
@@ -1,9 +1,283 @@
 package uj.wmii.pwj.introduction;
+import java.util.HashMap;
 
 public class Banner {
+    private final HashMap<Character, String[]> map;
 
-    public String[] toBanner(String input) {
-        return null;
+    public Banner () {
+        map = new HashMap<>();
+        createASCII();
     }
 
+    private void createASCII() {
+        map.put('A', new String[]{
+                "   #   ",
+                "  # #  ",
+                " #   # ",
+                "#     #",
+                "#######" ,
+                "#     #",
+                "#     #"
+        });
+        map.put('B', new String[]{
+                "###### ",
+                "#     #",
+                "#     #",
+                "###### ",
+                "#     #",
+                "#     #",
+                "###### "
+        });
+        map.put('C',new String[]{
+                " ##### ",
+                "#     #",
+                "#      ",
+                "#      ",
+                "#      ",
+                "#     #",
+                " ##### "
+        });
+        map.put('D', new String[]{
+                "###### ",
+                "#     #",
+                "#     #",
+                "#     #",
+                "#     #",
+                "#     #",
+                "###### "
+        });
+        map.put('E', new String[]{
+                "#######",
+                "#      ",
+                "#      ",
+                "#####  ",
+                "#      ",
+                "#      ",
+                "#######"
+        });
+        map.put('F', new String[]{
+                "#######",
+                "#      ",
+                "#      ",
+                "#####  ",
+                "#      ",
+                "#      ",
+                "#      "
+        });
+        map.put('G', new String[]{
+                " ##### ",
+                "#     #",
+                "#      ",
+                "#  ####",
+                "#     #",
+                "#     #",
+                " ##### "
+        });
+        map.put('H', new String[]{
+                "#     #",
+                "#     #",
+                "#     #",
+                "#######",
+                "#     #",
+                "#     #",
+                "#     #"
+        });
+        map.put('I', new String[]{
+                "###",
+                " # ",
+                " # ",
+                " # ",
+                " # ",
+                " # ",
+                "###"
+        });
+        map.put('J', new String[]{
+                "      #",
+                "      #",
+                "      #",
+                "      #",
+                "#     #",
+                "#     #",
+                " ##### "
+        });
+        map.put('K', new String[]{
+                "#    #",
+                "#   # ",
+                "#  #  ",
+                "###   ",
+                "#  #  ",
+                "#   # ",
+                "#    #"
+        });
+        map.put('L', new String[]{
+                "#      ",
+                "#      ",
+                "#      ",
+                "#      ",
+                "#      ",
+                "#      ",
+                "#######"
+        });
+        map.put('M', new String[]{
+                "#     #",
+                "##   ##",
+                "# # # #",
+                "#  #  #",
+                "#     #",
+                "#     #",
+                "#     #"
+        });
+        map.put('N', new String[]{
+                "#     #",
+                "##    #",
+                "# #   #",
+                "#  #  #",
+                "#   # #",
+                "#    ##",
+                "#     #"
+        });
+        map.put('O', new String[]{
+                "#######",
+                "#     #",
+                "#     #",
+                "#     #",
+                "#     #",
+                "#     #",
+                "#######"
+        });
+        map.put('P', new String[]{
+                "###### ",
+                "#     #",
+                "#     #",
+                "###### ",
+                "#      ",
+                "#      ",
+                "#      "
+        });
+        map.put('Q', new String[]{
+                " ##### ",
+                "#     #",
+                "#     #",
+                "#     #",
+                "#   # #",
+                "#    # ",
+                " #### #"
+        });
+        map.put('R', new String[]{
+                "###### ",
+                "#     #",
+                "#     #",
+                "###### ",
+                "#   #  ",
+                "#    # ",
+                "#     #"
+        });
+        map.put('S', new String[]{
+                " ##### ",
+                "#     #",
+                "#      ",
+                " ##### ",
+                "      #",
+                "#     #",
+                " ##### "
+        });
+        map.put('T', new String[]{
+                "#######",
+                "   #   ",
+                "   #   ",
+                "   #   ",
+                "   #   ",
+                "   #   ",
+                "   #   "
+        });
+        map.put('U', new String[]{
+                "#     #",
+                "#     #",
+                "#     #",
+                "#     #",
+                "#     #",
+                "#     #",
+                " ##### "
+        });
+        map.put('W', new String[]{
+                "#     #",
+                "#  #  #",
+                "#  #  #",
+                "#  #  #",
+                "#  #  #",
+                "#  #  #",
+                " ## ## "
+        });
+        map.put('V', new String[]{
+                "#     #",
+                "#     #",
+                "#     #",
+                "#     #",
+                " #   # ",
+                "  # #  ",
+                "   #   "
+        });
+        map.put('X', new String[]{
+                "#     #",
+                " #   # ",
+                "  # #  ",
+                "   #   ",
+                "  # #  ",
+                " #   # ",
+                "#     #"
+        });
+        map.put('Y', new String[]{
+                "#     #",
+                " #   # ",
+                "  # #  ",
+                "   #   ",
+                "   #   ",
+                "   #   ",
+                "   #   "
+        });
+        map.put('Z', new String[]{
+                "#######",
+                "     # ",
+                "    #  ",
+                "   #   ",
+                "  #    ",
+                " #     ",
+                "#######"
+        });
+        map.put(' ', new String[]{
+                "  ",
+                "  ",
+                "  ",
+                "  ",
+                "  ",
+                "  ",
+                "  "
+        });
+    }
+
+    public String[] toBanner(String input) {
+        if (input == null || input.isEmpty()) {
+            return new String[0];
+        }
+
+        input = input.toUpperCase();
+        final int height = 7;
+        String[] result = new String[height];
+
+        for (int i = 0; i < height; i++) {
+            StringBuilder row = new StringBuilder();
+            for (int j = 0; j < input.length(); j++) {
+                char c = input.charAt(j);
+                String[] letter = map.getOrDefault(c, map.get(' '));
+                row.append(letter[i]);
+
+                if (j < input.length() - 1) {
+                    row.append(" ");
+                }
+            }
+            result[i] = row.toString();
+        }
+
+        return result;
+    }
 }
--- src/main/java/uj/wmii/pwj/introduction/Reverser.java
+++ src/main/java/uj/wmii/pwj/introduction/Reverser.java
@@ -3,11 +3,24 @@ package uj.wmii.pwj.introduction;
 public class Reverser {
 
     public String reverse(String input) {
-        return null;
+        if (input == null) return null;
+        String trimmed = input.trim();
+        return new StringBuilder(trimmed).reverse().toString();
     }
 
     public String reverseWords(String input) {
-        return null;
-    }
+        if (input == null) return null;
 
+        String trimmed = input.trim();
+        String[] words = trimmed.split("\\s+");
+
+        StringBuilder result = new StringBuilder();
+        result.append(words[words.length - 1]);
+
+        for (int i = words.length - 2; i >= 0; i--) {
+            result.append(" ").append(words[i]);
+        }
+
+        return result.toString();
+    }
 }
